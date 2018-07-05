e8ce57efce7ddf25025c9ed2760608da1e784e0b
QAlexBall <zhuderen12345@icloud.com>
2018-07-05 23:44:43 +0800

    update

--- src/chapter03/BinarySearchST.java
+++ src/chapter03/BinarySearchST.java
@@ -1,5 +1,41 @@
 package chapter03;
+/**
+ * 基于有序数组
+ */
 
-public class BinarySearchST {
-    
+import static edu.princeton.cs.algs4.BinaryStdIn.isEmpty;
+
+public class BinarySearchST<Key extends  Comparable<Key>, Value> {
+    private Key[] keys;
+    private Value[] vals;
+    private int N;
+
+    public BinarySearchST(int capacity) {
+        keys = (Key[]) new Comparable[capacity];
+        vals = (Value[]) new Object[capacity];
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public Value get(Key key) {
+        if (isEmpty()) return null;
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) return vals[i];
+        else return null;
+    }
+    public int rank(Key key) {
+        return 0;
+    }
+
+    public void put(Key key, Value val) {
+        int i = 0;
+        for(int j = N; j > i; j--) {
+            keys[j] = keys[j - 1];
+            vals[j] = vals[j - 1];
+            keys[i] = key;
+            vals[i] = val;
+        }
+    }
 }
