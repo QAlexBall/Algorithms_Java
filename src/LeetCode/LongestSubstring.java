2034dfe215f15c5a4cc67b69234130a9309cdc4b
QAlexBall <zhuderen12345@icloud.com>
2018-07-21 20:21:13 +0800

    update

--- src/LeetCode/LongestSubstring.java
+++ src/LeetCode/LongestSubstring.java
@@ -1,6 +1,8 @@
 package LeetCode;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstring {
@@ -19,9 +21,24 @@ public class LongestSubstring {
         }
         return ans;
     }
+
+    private static int lengthOfLongestSubstring1(String s) {
+        int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        int ans = 0;
+        for(int i = 0, j = 0; i < n; i++) {
+            if(map.containsKey(s.charAt(i))) {
+                j = Math.max(map.get(s.charAt(i)), j);
+            }
+            ans = Math.max(ans, i - j + 1);
+            map.put(s.charAt(i), i + 1);
+        }
+        return ans;
+    }
     public static void main(String[] args) {
-        String s = "abbbbbcf";
+        String s = "abbbbbcfe";
         System.out.println(lengthOfLongestSubstring(s));
+        System.out.println(lengthOfLongestSubstring1(s));
     }
 }
 
